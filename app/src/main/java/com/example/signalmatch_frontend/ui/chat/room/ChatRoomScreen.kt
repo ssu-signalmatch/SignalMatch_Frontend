e76b263f1dc2c8f69ab092876939d57705349546
giyukim <giyu1020@gmail.com>
2025-12-01 00:53:26 +0900

    chore: add navController to Chat Room Screen Component

--- app/src/main/java/com/example/signalmatch_frontend/ui/chat/room/ChatRoomScreen.kt
+++ app/src/main/java/com/example/signalmatch_frontend/ui/chat/room/ChatRoomScreen.kt
@@ -10,17 +10,22 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
 import java.time.LocalDateTime
 
 
 @Preview
 @Composable
 fun ChatRoomScreenPreview () {
-    ChatRoomScreen()
+    val navController: NavHostController = rememberNavController()
+
+    ChatRoomScreen(navController = navController)
 }
 
 @Composable
-fun ChatRoomScreen () {
+fun ChatRoomScreen (navController: NavController) {
     Surface (
         modifier = Modifier.fillMaxSize(),
         color = Color.White
@@ -43,86 +48,6 @@ fun ChatRoomScreen () {
             ) {
                 ChatRoomContentContainer (
                     chatItems = arrayListOf(
-                        ChatItem(
-                            chatId = "1",
-                            sender = ChatItemSender.RECEIVE,
-                            type = ChatItemType.TEXT,
-                            userId = "1",
-                            name = "코끼리12",
-                            message = "안녕하세요~ 코끼리 1 입니다.",
-                            timestamp = LocalDateTime.of(2025, 11, 21, 9, 43, 30),
-                            read = true
-                        ),
-                        ChatItem(
-                            chatId = "2",
-                            sender = ChatItemSender.RECEIVE,
-                            type = ChatItemType.TEXT,
-                            userId = "1",
-                            name = "코끼리12",
-                            message = "문의 드리고 싶은 사항이 있어 연락드립니다~",
-                            timestamp = LocalDateTime.of(2025, 11, 21, 9, 43, 30),
-                            read = true
-                        ),
-                        ChatItem(
-                            chatId = "3",
-                            sender = ChatItemSender.SEND,
-                            type = ChatItemType.TEXT,
-                            userId = "2",
-                            name = "코끼리13",
-                            message = "안녕하세요~ 무슨일 때문에 연락하셨을까요?",
-                            timestamp = LocalDateTime.of(2025, 11, 21, 9, 43, 30),
-                            read = true
-                        ),
-                        ChatItem(
-                            chatId = "4",
-                            sender = ChatItemSender.RECEIVE,
-                            type = ChatItemType.TEXT,
-                            userId = "1",
-                            name = "코끼리12",
-                            message = "끼이끼끼끼끼끼",
-                            timestamp = LocalDateTime.of(2025, 11, 21, 9, 43, 30),
-                            read = true
-                        ),
-                        ChatItem(
-                            chatId = "1",
-                            sender = ChatItemSender.RECEIVE,
-                            type = ChatItemType.TEXT,
-                            userId = "1",
-                            name = "코끼리12",
-                            message = "안녕하세요~ 코끼리 1 입니다.",
-                            timestamp = LocalDateTime.of(2025, 11, 21, 9, 43, 30),
-                            read = true
-                        ),
-                        ChatItem(
-                            chatId = "2",
-                            sender = ChatItemSender.RECEIVE,
-                            type = ChatItemType.TEXT,
-                            userId = "1",
-                            name = "코끼리12",
-                            message = "문의 드리고 싶은 사항이 있어 연락드립니다~",
-                            timestamp = LocalDateTime.of(2025, 11, 21, 9, 43, 30),
-                            read = true
-                        ),
-                        ChatItem(
-                            chatId = "3",
-                            sender = ChatItemSender.SEND,
-                            type = ChatItemType.TEXT,
-                            userId = "2",
-                            name = "코끼리13",
-                            message = "안녕하세요~ 무슨일 때문에 연락하셨을까요?",
-                            timestamp = LocalDateTime.of(2025, 11, 21, 9, 43, 30),
-                            read = true
-                        ),
-                        ChatItem(
-                            chatId = "4",
-                            sender = ChatItemSender.RECEIVE,
-                            type = ChatItemType.TEXT,
-                            userId = "1",
-                            name = "코끼리12",
-                            message = "끼이끼끼끼끼끼",
-                            timestamp = LocalDateTime.of(2025, 11, 21, 9, 43, 30),
-                            read = true
-                        ),
                         ChatItem(
                             chatId = "1",
                             sender = ChatItemSender.RECEIVE,
